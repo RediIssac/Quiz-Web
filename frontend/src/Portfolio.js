77c298311fe91af1dcd002256991196dd793ea7a
Pyungkang Hong <pyungkang1004@gmail.com>
2020-03-18 02:49:17 +0900

    Update

--- frontend/src/Portfolio.js
+++ frontend/src/Portfolio.js
@@ -1,14 +1,17 @@
 // import React, {useState} from 'react';
 import GoogleLogin from "react-google-login";
 import './bootstrap.min.css'
+import logo from './logo.svg'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './App.css';
 // import { useHistory } from "react-router-dom";
-
+// import React from 'react';
+import Button from '@material-ui/core/Button';
 
 //데이터 베이스에서 아이디랑 프로파일을 가져와야함 
 import React, { Component } from 'react';
+
 
 
 class Portfolio extends Component {
@@ -29,14 +32,26 @@ class Portfolio extends Component {
         const { email, password, name } = this.state // Destructuring
 
         return (
-            <div>
-                <h1>Profile</h1>
+
+            <div className="Portfolio">
+
+
+                <div className="align-items-Right cad-n"></div>
+                <h4>Profile</h4>
                 <dt>Email</dt>
                 <dd>{email}</dd>
                 <dt>Password</dt>
                 <dd>{password}</dd>
                 <dt>Name</dt>
                 <dd>{name}</dd>
+                <header>
+                    <form id="to-do-form">
+                        <input type="text" placeholder="Type Account" />
+                        <input type="text" placeholder="Type password" />
+                        <input type="text" placeholder="Type your Name" />
+                        <button type="Edit">Edit</button>
+                    </form>
+                </header>
 
             </div>
         );
