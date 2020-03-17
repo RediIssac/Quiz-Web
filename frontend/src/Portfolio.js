6b582ab2816b89c833a51a899f3a65301745f450
Pyungkang Hong <pyungkang1004@gmail.com>
2020-03-17 23:58:17 +0900

    Update

--- frontend/src/Portfolio.js
+++ frontend/src/Portfolio.js
@@ -1,9 +1,9 @@
 // import React, {useState} from 'react';
-// import GoogleLogin from "react-google-login";
-// import './bootstrap.min.css'
+import GoogleLogin from "react-google-login";
+import './bootstrap.min.css'
 import axios from 'axios';
-// import { Link } from 'react-router-dom';
-// import './App.css';
+import { Link } from 'react-router-dom';
+import './App.css';
 // import { useHistory } from "react-router-dom";
 
 
