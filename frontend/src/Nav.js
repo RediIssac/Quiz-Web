1214c28b44e20102f7913c2ff20b2a001249ae7e
Pyungkang Hong <pyungkang1004@gmail.com>
2020-03-18 15:32:46 +0900

    Update

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -4,69 +4,84 @@ import './App.css';
 import { Link } from 'react-router-dom'
 
 function Nav() {
-    const navstyle = {
-        color: 'white'
-    }
-    var loggedin;
+  const navstyle = {
+    color: 'white'
+  }
+  var loggedin;
 
-    if (localStorage.getItem("loggedin") === null) {
-      loggedin = false;
-      localStorage.setItem("loggedin", false);
-    }
-    else{
+  if (localStorage.getItem("loggedin") === null) {
+    loggedin = false;
+    localStorage.setItem("loggedin", false);
+  }
+  else {
 
-      loggedin = localStorage.getItem('loggedin');
+    loggedin = localStorage.getItem('loggedin');
 
-    }
-    
-    
-    const logout = () => {
-      localStorage.removeItem('loggedin');
-      loggedin = false;
-      localStorage.removeItem('tokenId');
-      localStorage.removeItem('userCredentials');
-      
+  }
 
-    }
-  
-    return (
-    
+
+  const logout = () => {
+    localStorage.removeItem('loggedin');
+    loggedin = false;
+    localStorage.removeItem('tokenId');
+    localStorage.removeItem('userCredentials');
+
+
+  }
+
+  return (
+
     <nav >
-        <h3>Quiz-Web</h3>
-        <ul className = "nav-links">
-            <Link style= {navstyle} to="/" exact>
-            <li>Home</li>
+      <h3>Quiz-Web</h3>
+      <ul className="nav-links">
+        <Link style={navstyle} to="/" exact>
+          <li>Home</li>
+        </Link>
+        <Link style={navstyle} to="/Quizzes">
+          <li>Quizzes</li>
+        </Link>
+
+
+
+        {
+          !loggedin &&
+          (<div>
+            <Link style={navstyle} to="/Login">
+              <li>Login</li>
             </Link>
-            <Link style= {navstyle} to="/Quizzes">
-            <li>Quizzes</li>
+            <Link style={navstyle} to="/SignUp">
+              <li>SignUp</li>
             </Link>
-            
-            {
-                !loggedin &&
-            ( <div>                
-                  <Link style= {navstyle} to="/Login">
-                  <li>Login</li>
-                  </Link>
-                  <Link style= {navstyle} to="/SignUp">
-                  <li>SignUp</li>
-                  </Link>
-                  </div>
-      
-               )
+
+          </div>
+
+          )
 
 
-            }
+        }
 
 
 
-            {
-              loggedin &&
-              (
-            <button onClick={logout} value="LogOut"><li >LogOut</li>
-            </button>)
-            }
-           
-        </ul>
+        {
+          loggedin &&
+          (
+            <div>
+              <Link style={navstyle} to="/Portfolio">
+                <li>Portfolio</li>
+              </Link>
+
+
+              <button onClick={logout} value="LogOut"><li >LogOut</li>
+              </button>
+
+            </div>
+          )
+
+
+
+        }
+
+      </ul>
     </nav>
   );
 }
